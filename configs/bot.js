e5ac5bef1bb32509e2fa6f3120da9e55521fcd0a
Swaraj-17 <119113330+Swaraj-17@users.noreply.github.com>
2024-09-08 09:45:39 +0530

    Add folder content

--- configs/bot.js
+++ configs/bot.js
@@ -1,7 +1,11 @@
 require('dotenv').config();
 const TelegramBot = require('node-telegram-bot-api');
-const token = process.env.TOKEN;
+const token = '7422232451:AAFF_SLjeeSyhucfNay9zXB-0b33n6F5aoI';
 
-const bot = new TelegramBot(token, {polling: true});
+const bot = new TelegramBot('7422232451:AAFF_SLjeeSyhucfNay9zXB-0b33n6F5aoI', {polling: true});
+bot.on('message', (msg) => {
+    bot.sendMessage(msg.chat.id, 'Received your message');
+  });
+
 
 module.exports= bot;
--- configs/mongoose.js
+++ configs/mongoose.js
@@ -2,7 +2,14 @@ const mongoose = require('mongoose');
 
 let dbURI = process.env.DB_URL;
 
-mongoose.connect(dbURI);
+mongoose.connect('mongodb://127.0.0.1/ProductAlertify', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }).then(() => {
+    console.log('Connected to MongoDB');
+  }).catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+});
 
 const db = mongoose.connection;
 
